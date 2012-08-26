7b4be1394d9b3b72a622a8a1333cc3887d2e7c86
Tom Hogans <tomhsx@gmail.com>
2012-08-26 16:15:19 -0400

    Bump version number to 0.2

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.2'
 
 setup(name='webscraper',
       version=version,
       description="Web scraping tools",
-      long_description="""\
-Tools to make web scraping a more simple, less repetitive process.""",
+      long_description="""Tools to make web scraping a more simple, less repetitive process.""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='web scraping urllib2',
       author='Tom Hogans',
-      author_email='tomh@oh4.org',
+      author_email='tomhsx@gmail.com',
       url='https://github.com/tomhsx/webscraper',
-      license='',
+      license='See LICENSE file',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
